a23085ceda05c8fc35235d1f936d5de65aa9d1f0
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-21 02:06:19 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,11 +2,15 @@ class Triangle
   attr_accessor :kind
 
   def initialize(a, b, c)
-    if a == b && a == c
+    case self
+    when a == 0 || b == 0 || c == 0
+      raise TriangleError
+    end 
       @kind = :equilateral
 
     end
   end
 
-
+  class TriangleError < StandardError
+  end
 end
