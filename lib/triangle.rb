ce08be1adab0a7a726b8800f086478310c41d4a2
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-21 02:07:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,12 +2,12 @@ class Triangle
   attr_accessor :kind
 
   def initialize(a, b, c)
-    
+
     if a == 0 || b == 0 || c == 0
       raise TriangleError
     elsif a == b && a == c
       @kind = :equilateral
-    elsif
+
     end
   end
 
